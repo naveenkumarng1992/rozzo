d3d4fec66aea08bf9ba634c3693438fb5f21db49
naveenkumarng1992 <medmax3323@gmail.com>
2025-11-22 23:12:53 +0530

    Create GeneratedPluginRegistrant.swift

---
+++ ios/Runner/GeneratedPluginRegistrant.swift
@@ -0,0 +1,15 @@
+//
+//  Generated file. Do not edit.
+//
+
+import Flutter
+import UIKit
+
+public class GeneratedPluginRegistrant: NSObject {
+  @objc public static func register(with registry: FlutterPluginRegistry) {
+    // Registers all Flutter plugins automatically
+    if let registrar = registry.registrar(forPlugin: "GeneratedPluginRegistrant") {
+      // No direct plugin calls needed — Flutter's build process will auto-populate this
+    }
+  }
+}
